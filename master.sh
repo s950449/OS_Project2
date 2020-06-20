a080c73934e92e1e02272680e5b6459cf8e3cbae
s950449 <b07902125@ntu.edu.tw>
2020-06-20 22:44:53 +0800

    Add output

--- master.sh
+++ master.sh
@@ -2,5 +2,5 @@
 END=10
 for i in $(seq 1 $END);
 do
-    $PWD/user_program/master 1 fcntl $PWD/input/sample_input_1/target_file_$i
+    $PWD/user_program/master 1 fcntl $PWD/input/sample_input_1/target_file_$i | grep "Transmission time" >> $PWD/output/fcntl_master_result.txt
 done
--- slave.sh
+++ slave.sh
@@ -3,5 +3,5 @@ IP="127.0.0.1"
 END=10
 for i in $(seq 1 $END);
 do
-    $PWD/user_program/slave 1 fcntl $IP $PWD/output/target_file_$i
+    $PWD/user_program/slave 1 fcntl $IP $PWD/output/target_file_$i | grep "Transmission time" >> $PWD/output/fcntl_slave_result.txt
 done
