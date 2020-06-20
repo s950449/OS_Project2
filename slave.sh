241e0f849345517a62bc2332c66fd6e6bd2776dc
s950449 <b07902125@ntu.edu.tw>
2020-06-20 22:57:54 +0800

    Delete unneeded script

--- slave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-IP="127.0.0.1"
-END=10
-for i in $(seq 1 $END);
-do
-    $PWD/user_program/slave 1 fcntl $IP $PWD/received/target_file_$i | grep "Transmission time" >> $PWD/output/fcntl_slave_result.txt
-done
