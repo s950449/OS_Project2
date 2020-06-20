935719d9beb6cb062b1a0f7006eda6e2509fa2dc
s950449 <b07902125@ntu.edu.tw>
2020-06-20 10:36:22 +0800

    [Testing] Use fixed PAGE_SIZE

--- user_program/master.c
+++ user_program/master.c
@@ -12,6 +12,7 @@
 
 #define FILE_START_NUM 3
 #define BUF_SIZE 512
+#define PAGE_SIZE 4096
 #define P2_MAP_SIZE PAGE_SIZE * 100
 size_t get_filesize(const char* filename);//get the size of the input file
 
