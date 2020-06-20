5a5509b9639e9769e5260ff61943d19457de9fe0
s950449 <b07902125@ntu.edu.tw>
2020-06-20 11:00:24 +0800

    [Testing] Use io_remap_pfn_range instead of remap_pfn_range

--- slave_device/slave_device.c
+++ slave_device/slave_device.c
@@ -82,7 +82,7 @@ static int project2_mmap(struct file *file,struct vm_area_struct *vma){
 	phys_addr_t my_page;
 	my_page = virt_to_phys(file->private_data) >> PAGE_SHIFT;
 	my_vma_size = vma->vm_end-vma->vm_start;
-	remap_pfn_range(vma,vma->vm_start,my_page,my_vma_size,vma->vm_page_prot);
+	io_remap_pfn_range(vma,vma->vm_start,my_page,my_vma_size,vma->vm_page_prot);
 	vma->vm_ops = &project2_vm_ops;
 	vma->vm_flags |=VM_RESERVED;
 	vma->vm_private_data = file->private_data;
