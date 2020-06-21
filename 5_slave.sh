8119e464f8d2a9679da19abdc1a452305930e593
s950449 <b07902125@ntu.edu.tw>
2020-06-21 14:48:21 +0800

    Test running script

--- 5_slave.sh
+++ 5_slave.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 IP="127.0.0.1"
 END=10
-rm $PWD/output/5_$1_slave_result.txt
+#rm $PWD/output/5_$1_slave_result.txt
 for i in {1..10};
 do
     $PWD/user_program/slave 5 $1 $IP $PWD/output/target_file_6 $PWD/output/target_file_7 $PWD/output/target_file_8 $PWD/output/target_file_9 $PWD/output/target_file_10  | grep "Transmission time" >> $PWD/output/5_$1_slave_result.txt
